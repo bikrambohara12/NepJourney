2c175673d22c7a08d9209e8b4c84b24f755d2d87
amirbohara5 <amirbohara00@gmail.com>
2025-07-09 22:29:08 +0545

    Add payment schema

---
+++ models/payment.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  order: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Order',  // tyo collection jo sang payment link hunxs
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  paymentMethod: {
+    type: String, // e g creatad card, khalti, esewa etc
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'completed', 'failed', 'refunded'],
+    default: 'pending',
+  },
+  paymentDate: {
+    type: Date,
+    default: Date.now,
+  },
+  transactionId: {
+    type: String, // payment gateway bata aayeko transaction id
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Payment', paymentSchema);

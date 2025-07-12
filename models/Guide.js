5d0e4c964e319e399239d52fc59d27f2bc0d46a0
amirbohara5 <amirbohara00@gmail.com>
2025-07-12 09:49:41 +0545

    Updated guide and user schemas

--- models/Guide.js
+++ models/Guide.js
@@ -4,13 +4,59 @@ import mongoose from "mongoose";
 const guideSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true  // guide ko naam required ho
+    required: true  
   },
+  email: {
+    type: String,
+    required: true,  
+    unique: true 
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true  
+  }, 
+  speciality: {
+    type: String,
+    required: true
+  }, 
+  degree: {
+    type: String,
+    required: true  
+  },
+  about: {
+    type: String,
+    required: true  
+  },
+  available: {
+    type: Boolean,
+    required: true  
+  },
+  fee: {
+    type: Number,
+    required: true  
+  }, 
+  address: {
+    type: Object,
+    required: true  
+  }, 
+  Date: {
+    type: Number,
+    required: true  
+  },
+  slots_booked: {
+    type: Object,
+    default: {}  
+  },
+
   language: {
-    type: [String]     // guide le bolne language haru array form m axa
+    type: [String]     
   },
   experience: {
-    type: Number  // experience years
+    type: Number  
   }
 });
 
--- models/User.js
+++ models/User.js
@@ -16,11 +16,37 @@ const userSchema = new mongoose.Schema({
     required: true, // true vaneko required ho, false optional ho 
     minlength: 6   //minimum length validation
   },
+  gender: {
+    type: String,
+    required: true,
+    enum: ['male', 'female', 'other'],
+    default: 'not selected'
+  },
+
   role: {
     type: String,
     enum: ['admin', 'guide', 'user'],
     default: 'user'
-  }
+  },
+  dateofbirth: {
+    type: Date,
+    required: true,
+    default: "not selected"
+  },
+
+  image: {
+    type: String,
+    required: true
+  },
+  addresss : {
+    type: Object,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  
 });
 
 // Schema bata model banaune
